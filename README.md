0ae56be373a79d05ca4fc3455e023641fa538280
kiwidog <forums@allenthinks.com>
2015-01-04 14:08:35 -0500

    Inital code commit.

--- README.md
+++ README.md
@@ -2,3 +2,37 @@ RimeExamplePlugin
 =================
 
 Create your own tools to integrate into Rime.
+
+Currently the way that the plugin system works is...
+
+1. Rime checks .\plugins directory for any plugin with a .dll.
+2. Rime checks to see if it matches the IPlugin interface, if it does not then quietly returns.
+3. Plugin is stored for use in memory for a later time.
+
+A user requests to open content (done internally via Rime)
+
+1. The watchdog system will scan all available plugins by extension to see which ones can be loaded
+2. If there are matches then load the first available plugin (later on may be changed to have user selectable when multiple matches occur)
+3. Call IPlugin.Init() if Args are null (sent via Rime) or IPlugin.Init(object p_Object).
+4. If the plugin's initialization phase goes through successfully, then the plugin is sent off to the main window (with UI if there is one available)
+5. Rime's main UI will auto-add the UI to the main window if there is one available.
+
+Plugin Creators
+There are many things that will probably change in the upcoming releases of Rime.
+
+But for now, during the Init call's YOU the developer will have to do all error checking, type checking, and exception handling within the plugin.
+
+Reason being, Rime's plugin system does not have that strong of a hypervisor, watchdog, whatever you want to call it. So the entire app has the potential to crash from poorly written plugins.
+Personally, I will eventually get around to making it a bit stronger, more secure, and be able to interact with the application as well.
+
+Also, you WILL have to compile for x64, get over it. x86 libraries will not load in Rime. (Yes, Rime is x64 only...So are most newer FB games anyway)
+
+To add a new UI, the "Main Window" of your plugin will have to be a WPF User Control. You do not have to manually create them in code, just...
+
+1. Add a new WPF User Control in Visual Studio
+2. Name it something useful
+3. In whichever Init function you need, just call something similar to 'm_Control = new MyWpfUserControl();'
+
+From there, you can actually do all of your logic, loading, whatever from that user control, or from the plugin it does not matter to Rime.
+
+This has been a terrible readme, I am sorry and will eventually update the documentation for you to use.
---
+++ RimeExamplePlugin.cs
@@ -0,0 +1,129 @@
+﻿/*
+    The MIT License (MIT)
+
+    Copyright (c) 2005-2015 kiwidoggie productions
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// WPF User Control
+using System.Windows.Controls;
+
+// Plugin System
+using Rime.Core.Plugins;
+
+namespace RimePluginExample
+{
+    /// <summary>
+    /// Example plugin class for Rime.
+    /// </summary>
+    public class RimeExamplePlugin : IPlugin
+    {
+        // Set these to your current configuration
+        private string m_Name = "Example Plugin";
+        private string m_Author = "Rime Developers (kiwidog)";
+        private string m_Version = "0.01";
+        private string m_Description = "A example plugin.";
+        private string m_Extension = ".ext";
+        private UserControl m_Control = null;
+
+        /// <summary>
+        /// Initialization with no parameters
+        /// </summary>
+        /// <returns>True for success, False for failure</returns>
+        public bool Init()
+        {
+            System.Windows.MessageBox.Show("Init called.");
+            return true;
+        }
+
+        /// <summary>
+        /// Initalization with one parameter, plugin will have to do all type and cast checking itself.
+        /// </summary>
+        /// <param name="p_Object">Object passed as argument</param>
+        /// <returns>True for success, False for failure</returns>
+        public bool Init(object p_Object)
+        {
+            // Handle if one object is passed, check SDK for what is passed
+            System.Windows.MessageBox.Show("Init with one parameter called.");
+            return true;
+        }
+
+        /// <summary>
+        /// Initialization with multiple parameters, plugin will have to do all type and cast checking itself.
+        /// </summary>
+        /// <param name="p_Objects">Array of objects passed as arguments</param>
+        /// <returns>True for success, False for failure</returns>
+        public bool Init(object[] p_Objects)
+        {
+            // Handle multiple plugin objects coming in at once
+            System.Windows.MessageBox.Show("Init with multiple paramerters called.");
+            return true;
+        }
+
+        /// <summary>
+        /// Close is called when the plugin is being destroyed, free all memory, ui, resources here.
+        /// </summary>
+        public void Close()
+        {
+            // TODO: Call cleanup if nessessary
+        }
+
+        public bool UIEnabled
+        {
+            get { return (m_Control != null); }
+        }
+
+        public string Name
+        {
+            get { return m_Name; }
+        }
+
+        public string Author
+        {
+            get { return m_Author; }
+        }
+
+        public string Version
+        {
+            get { return m_Version; }
+        }
+
+        public string Description
+        {
+            get { return m_Description; }
+        }
+
+        public UserControl MainControl
+        {
+            get { return m_Control; }
+        }
+
+        public string Extension
+        {
+            get { return m_Extension; }
+        }
+    }
+}
