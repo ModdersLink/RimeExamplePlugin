d182ab811cd3cf8524c229b492626951ac39903c
kiwidog <forums@allenthinks.com>
2015-11-05 10:56:44 -0500

    Updated to the 1.3-alpha format of plugins.

--- RimeExamplePlugin.cs
+++ RimeExamplePlugin.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows.Controls;
+using RimeCommon.Content.Messages;
 using RimeCommon.Logging;
+using RimeCommon.Messaging;
 using RimeCommon.Plugins;
 
 namespace RimePluginExample
@@ -12,16 +14,22 @@ namespace RimePluginExample
         public override string Description { get { return "An description for the example plugin, hmmm.... what should it be?"; } }
         public override string Extension { get { return "_rime-plugin-example"; } }
         public override UserControl MainControl { get { return null; } }
-        public override MountPoint Mount { get { return MountPoint.Center; } }
+        public override MountPoint Mount { get { return MountPoint.None; } }
 
-        public RimeExamplePlugin()
+        public override void Init(params object[] p_Params)
         {
-            WriteLog(LogsLevel.All, "RimeExamplePlugin ctor called.");
+            RegisterListener(MessagingSubSystem.Content);
+
+            RegisterMessageCallback(typeof(ContentRequestOpen), OnContentRequestOpen);
+
+            WriteLog(LogsLevel.All, "RimeExamplePlugin Init called.");
         }
 
-        public override void Init()
+        private void OnContentRequestOpen(RimeMessage p_Message)
         {
-            
+            var s_Message = (ContentRequestOpen) p_Message;
+
+            WriteLog(LogsLevel.All, "Swag, Swag, Swag, Swag");
         }
     }
 }
