7ea00b009f9efedf17038f48dd23ace4bca1b696
kiwidog <forums@allenthinks.com>
2016-09-17 00:30:31 -0400

    Removal of WPF dependencies.
    Added WinForms dependencies.
    
    Upgraded solution to Visual Studio 2015.

--- RimeExamplePlugin.cs
+++ RimeExamplePlugin.cs
@@ -1,8 +1,6 @@
-﻿using System.Windows.Controls;
-
+﻿using System.Windows.Forms;
 using RimeCommon.Plugins;
 using RimeCommon.Messaging;
-using RimeCommon.Messages;
 using RimeCommon.Content.Messages;
 using RimeCommon.Logging;
 
@@ -10,13 +8,13 @@ namespace RimePluginExample
 {
     public class RimeExamplePlugin : RimePlugin
     {
-        public override string Name { get { return "Rime Example Plugin"; } }
-        public override string Author { get { return "Example Author"; } }
-        public override string Version { get { return "1.0"; } }
-        public override string Description { get { return "An description for the example plugin, hmmm.... what should it be?"; } }
-        public override string Extension { get { return "_rime-plugin-example"; } }
-        public override UserControl MainControl { get { return null; } }
-        public override MountPoint Mount { get { return MountPoint.None; } }
+        public override string Name => "Rime Example Plugin";
+        public override string Author => "Example Author";
+        public override string Version => "1.0";
+        public override string Description => "An description for the example plugin, hmmm.... what should it be?";
+        public override string Extension => "_rime-plugin-example";
+        public override UserControl MainControl => null;
+        public override MountPoint Mount => MountPoint.None;
 
         public override void Init(params object[] p_Params)
         {
